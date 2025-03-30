054f2d46771e2a5505c9be98a7d96ef7e57af5a4
Tibor Vancza <tibor.vancza@accenture.com>
2025-03-30 14:55:45 +0300

    new function added with pylint fixes

---
+++ app.py
@@ -0,0 +1,10 @@
+"""
+This is a new file created to demonstrate the use of comments and functions.
+"""
+
+def new_function():
+    """
+    This function prints a message and returns a string.
+    """
+    print("This is a new function.")
+    return "Function executed successfully."
